188a1364f66fa46c15a28838c6e7240851c78605
agmeadows <65190360+agmeadows@users.noreply.github.com>
2022-02-12 11:19:30 -0500

    Test site

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello World!"
+    return render_template("index.html")
