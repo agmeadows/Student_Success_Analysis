14a877c1bed03ea4c8dff4e53d718c00ccc68f9b
agmeadows <65190360+agmeadows@users.noreply.github.com>
2022-02-13 21:17:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+from flask_wtf.recaptcha import RecaptchaField
 
 from flask_wtf import FlaskForm, RecaptchaField
 from wtforms import SubmitField, SelectField, RadioField, HiddenField, StringField, IntegerField, FloatField
 from wtforms.validators import InputRequired, Length, Regexp, NumberRange
+
 from datetime import datetime
 from uuid import uuid4
 
